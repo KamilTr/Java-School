bf5a4ca33415a62cb7a5c9172c6e080d0ecd38d3
Kamil <Kamil@Kamil-PC.lan>
2017-04-12 03:40:22 +0200

    FH

--- Inlämning_3/src/u2/SimpleLinkedList.java
+++ Inlämning_3/src/u2/SimpleLinkedList.java
@@ -2,22 +2,16 @@ package u2;
 
 import java.util.*;
 
-/**
- * Klassen SimpleLinkedList är en länkad lista som innehåller noder av objekt.
- * Första noden i listan innehåller inget objekt.
- */
 
-public class SimpleLinkedList {
+public class SimpleLinkedList{
 	private ListNode header;
+	private int listSize = 0;
 
-	public SimpleLinkedList() {
+	public SimpleLinkedList(){
 		header = new ListNode(null);
 	}
 
-	/**
-	 * Skapar en ny ListNode med ett nytt objekt och lägger den i listan
-	 */
-	public void insert(String theobj) {
+	public void insert(String theobj){
 		ListNode nynode = new ListNode(theobj);
 
 		ListNode temp = header;
@@ -26,12 +20,10 @@ public class SimpleLinkedList {
 		}
 
 		temp.next = nynode;
+		listSize++;
 	}
 
-	/*
-	 * Ta bort noden som innehåller respektive objekt
-	 */
-	public void remove(String theobj) {
+	public void remove(String theobj){
 		ListNode node = header;
 
 		while (node.next != null) {
@@ -41,12 +33,10 @@ public class SimpleLinkedList {
 			}
 			node = node.next;
 		}
+		listSize--;
 	}
 
-	/**
-	 * Skriver ut innehållet i listan
-	 */
-	public void print() {
+	public void print(){
 		ListNode node = header.next;
 		while (node != null) {
 			System.out.println(node.element);
@@ -54,45 +44,72 @@ public class SimpleLinkedList {
 		}
 	}
 
-	public void insert2(String theobj) {
+	public void insert2(String theobj){
 		ListNode nynode = new ListNode(theobj, header.next);
 		header.next = nynode;
 	}
 
-	// Detta skall du göra
-	public void insert(String obj, int index) {
-
+	public void insert(String obj, int index){
+		ListNode temp = new ListNode(obj);
+		ListNode cur = header;
+		int counter = 0;
+		while(cur.next!=null){
+			cur= cur.next;
+			counter++;
+			if(counter == index){
+				temp.next = cur.next;
+				cur.next = temp;
+			}
+		}
+		listSize++;
+		
+	}
+	
+	public String remove(String obj, int index){
+		ListNode cur = header;
+		int counter = 0;
+		while (cur.next != null) {
+			if (cur.next.element.equals(obj) && counter!=index) {return null;}
+			else if (cur.next.element.equals(obj) && cur.next.next!=null && counter==index) {cur.next = cur.next.next;}
+			else{cur = cur.next;counter++;}
+		}
+		listSize--;
+		return obj;
 	}
 
-	// Returnerar och tar bort ett objkt från lista
-	public String remove(String obj, int index) {
-		// temporär return
-		return null;
+	public int size(){
+		return listSize;
 	}
 
-	// returnerar storleken på listan. Lägg till i klasssen en variabel för
-	// detta
-	// uppdatera variabeln när det är fallet
-
-	public int size() {
-		// temporär return
-
-		return 0;
-	}
-
-	public String toString() {
-
-		// temporär return
-		return null;
+	public String toString(){
+		String anwser = "";
+		ListNode cur = header;
+		while(cur.next != null){
+			anwser = anwser + cur.next.element + " ";
+			cur = cur.next;
+		}
+		return anwser;
 	}
 
 	public static void main(String[] arg) {
 		// testa metoderna från klassen SimpleLinkedList
 		SimpleLinkedList klassLista = new SimpleLinkedList();
 		klassLista.insert("Olle");
-		klassLista.insert2("Nina");
+		klassLista.insert("Anna");
+		klassLista.insert("Nina");
+		klassLista.insert("Johan");
+		klassLista.insert("Kamil");
+		klassLista.insert("Kalle", 1);
 		klassLista.print();
-		klassLista.remove("Nina");
+		System.out.println(klassLista.size());
+		
+		System.out.println();
+		System.out.println(klassLista.remove("Olle", 0) + " was removed\n");
+
 		klassLista.print();
+		System.out.println(klassLista.size());
+		
+		System.out.println(klassLista.toString());
+		
 	}
 }
--- Inlämning_3/src/u3/ItemList.java
+++ Inlämning_3/src/u3/ItemList.java
@@ -1,88 +1,107 @@
 package u3;
 
+import java.util.*;
 
-   import java.util.*;
 
 /**
  * Klassen ItemList är en länkad lista som innehåller noder av Item-objekt.
  * Första noden i listan ( header) innehåller inget objekt.
  */
 
-   public class ItemList{
-      private Node header;
-      int size;
-    
-      public ItemList( ) {
-         header = new Node( null );
-      }
-    
-    /**
-    Skapar ett ny Node-objekt med ett nytt Item objekt och lägger den i listan. 
-    Ökar värdet av variabel size;
-     */
-      public void add(Item newItem) {
-      
-      
-      }
-    
-    /**
-    Skapar ett ny Node objekt med ett nytt Item-objekt och lägger den i listan på plats index,
-    Ökar värdet av variabel size;
-   */
-      public void add( Item newItem, int index)
-      {
-      
-      
-      }
-    
-    
-    /**
-    Returnerar true om det finns ett Item-objekt vars RFIDNR är lika med metodens parametern id 
-     */
-      public boolean find(String id) {
-          
-         return true;	 
-      }
-    
-   /**    
-       Ta bort noden som innehåller Item- objektet med viss id*/
-      public Item remove(String id) {
-      
-         return null;
-      }
-   
-    /**
-     Räknar och returnerar antalet Item- objekt med ett vis namn oavsett RFIDNR 
-     */
-   
-      public int countObjects(String name) {
-      
-         return 0;
-      }
-   
-    
-    /**
-    Skriver ut innehållet i listan
-    */
-      public void printList() {
-      }
-    
-    /**
-     Returnera true om listan är tom annars false
-    */
-      public boolean isEmpty()
-      {
-         return true;
-      }
-		
-		/**
-     Returnera antlet element i listan
-    */
-      public int size()
-      {
-         return 0;
-      }
+public class ItemList {
+	private Node header;
+	int size;
 
-    
-        
-   }
+	public ItemList() {
+		header = new Node(null);
+	}
+
+	/**
+	 * Skapar ett ny Node-objekt med ett nytt Item objekt och lägger den i
+	 * listan. Ökar värdet av variabel size;
+	 */
+	public void add(Item newItem) {
+		Node temp = header;
+		Node newNode = new Node(newItem);
+		while(temp.next != null){
+			temp = temp.next;
+		}
+		temp.next = newNode;
+		size++;
+	}
+
+	/**
+	 * Skapar ett ny Node objekt med ett nytt Item-objekt och lägger den i
+	 * listan på plats index, Ökar värdet av variabel size;
+	 */
+	public void add(Item newItem, int index) {
+		Node temp = new Node(newItem);
+		Node cur = header;
+		int counter = 0;
+		while(cur.next!=null){
+			cur = cur.next;
+			counter++;
+			if(counter == index){
+				temp.next = cur.next;
+				cur.next = temp;
+			}
+		}
+		size++;
+	}
+
+	/**
+	 * Returnerar true om det finns ett Item-objekt vars RFIDNR är lika med
+	 * metodens parametern id
+	 */
+	public boolean find(String id) {
+		Node temp = header;
+		while(temp.next != null)
+			if(temp.next.itm.getItemNumber()==id)
+				return true;
+		return false;
+	}
+
+	/**
+	 * Ta bort noden som innehåller Item- objektet med viss id
+	 */
+	public Item remove(String id) {
+
+		return null;
+	}
+
+	/**
+	 * Räknar och returnerar antalet Item- objekt med ett vis namn oavsett
+	 * RFIDNR
+	 */
+
+	public int countObjects(String name) {
+		Node temp = header;
+		int count = 0;
+		while(temp.next!=null){
+			if(temp.next.itm.getItemName()==name)
+				count++;
+		}
+		return count;
+	}
+
+	/**
+	 * Skriver ut innehållet i listan
+	 */
+	public void printList() {
+	}
+
+	/**
+	 * Returnera true om listan är tom annars false
+	 */
+	public boolean isEmpty() {
+		return true;
+	}
+
+	/**
+	 * Returnera antlet element i listan
+	 */
+	public int size() {
+		return 0;
+	}
+
+}
